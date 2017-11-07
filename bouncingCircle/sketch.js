399df7dd026e8f8846a71c0ccea985960a2a0968
mechanicallyextendedman <33380459+mechanicallyextendedman@users.noreply.github.com>
2017-11-07 03:06:32 -0400

    Add files via upload

---
+++ bouncingCircle/sketch.js
@@ -0,0 +1,33 @@
+var x = 0;
+var y = 0;
+var directionX = 5;
+var directionY = 5;
+
+
+function setup() {
+  // put setup code here
+    createCanvas(800, 600);
+        background(0);
+
+}
+
+function draw() {
+  // put drawing code here
+    
+    fill(random(0,255),random(0,255),random(0,255)), 25;
+    ellipse(x, y, 80, 80);
+move();
+}
+
+
+function move(){
+    if(x < 0 || x > width){
+        directionX = directionX * -1;
+    }
+    if(y < 0 || y > height){
+        directionY = directionY * -1;
+    }
+    x = x + directionX;
+    y = y + directionY;
+
+}
